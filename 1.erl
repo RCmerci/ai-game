383933bf6615743e37433849fc303e8f52b736a4
RCmerci <562837353@qq.com>
2014-08-02 00:50:33 +0800

    go on

--- 1.erl
+++ 1.erl
@@ -32,9 +32,12 @@ start()->
     {ok, S} = gen_tcp:listen(10000, [list, {active, false}]),
     {ok, AccSock} = get_connect(S, 2, []),
     loop_for_every_one(AccSock),
+    {ok, SS} = gen_tcp:listen(10001, [list, {active, false}]),
+    {ok, GetInfoSock} = get_connect(SS, 2, []),
+    spawn(?MODULE, loop_for_get_info, [GetInfoSock]),
     ok.
 
-get_connect(Socket, 0, AccSock)-> {ok, AccSock};
+get_connect(_Socket, 0, AccSock)-> {ok, AccSock};
 get_connect(Socket, Num, AccSock)->
     case gen_tcp:accept(Socket) of
         {ok, SS} ->
@@ -48,9 +51,35 @@ get_connect(Socket, Num, AccSock)->
 
 loop_for_every_one([]) -> ok;
 loop_for_every_one([Sock|Rest]) ->
-    spawn(?MODULE, loop, [Sock]),
+    spawn(?MODULE, loop_for_set, [Sock]),
     loop_for_every_one(Rest).
 
+
+loop_for_set(Socket)->
+    case gen_tcp:recv(Socket, 0) of
+        {ok, Content} ->
+            handle_set(Socket, Content),
+            loop_for_set(Socket);
+        _ ->
+            io:format("error in fun[loop/1]"),
+            exit('error_in_fun[loop/1]')
+        end,
+    ok.
+
+loop_for_get_info(Socket) ->
+    sub_loop_for_get_info(Socket, Socket).
+
+sub_loop_for_get_info([], All) ->
+    sub_loop_for_get_info(All, All);
+sub_loop_for_get_info([Socket|Rest], All) ->
+    %% 0.2秒等待
+    case gen_tcp:recv(Socket, 0, 200) of
+        {ok, Content} ->
+            handle_get(Socket, Content),
+            sub_loop_for_get_info(Rest, All);
+        {error, timeout} ->
+            sub_loop_for_get_info(Rest, All)
+    end.
 
 %%%%%%%%%%%%%%%%%%%%%%
 %tcp 消息格式：
@@ -58,22 +87,57 @@ loop_for_every_one([Sock|Rest]) ->
 %% '{set,{name, direction, attack}, {name_2, ...}, ....};:
 %%见record
 %%%%%%%%%%%%%%%%%%%%%%
-loop(Socket)->
-    case gen_tcp:recv(Socket, 0) of
-        {ok, Content} ->
-            handle_set(Socket, Content),
-            loop(Socket);
-        _ ->
-            io:format("error in fun[loop/1]"),
-            exit('error_in_fun[loop/1]')
-        end,
-    ok.
 
 %%处理'set'类请求
 handle_set(Socket, Content) ->
+    ContentList = handle_content(Content),
+    [Set|RestList] = ContentList,
+    if
+        Set=="set" ->
+            sub_handle_set(Socket,RestList);
+        true ->
+            io:format("set 类请求格式错误~n~p~n", [Content])
+    end,
     ok.
 
+%% 处理Content的内容,分割成list
+handle_content(Content) ->
+    InitList = string:tokens(Content, "{}"),
+    lists:filter(fun(X)->length(string:strip(X))>1 end, InitList).
 
+%% 真正处理set request的函数
+sub_handle_set(Socket, ContentList) ->
+    ok.                                         %明天写= =
+
+%%处理‘get’类请求
+handle_get(Socket, Content) ->
+    if
+        "{get_info}" == Content ->
+            sub_handle_get(Socket),
+            ok;
+        true -> io:format("dont match ‘{get_info}’")
+    end,
+    ok.
+
+sub_handle_get(Socket) ->
+    Key = ets:first(person_info),
+    Res = ets:lookup(person_info, Key),
+    Result = lists:flatten(io_lib:format("~p", gather(Res))),
+    gen_tcp:send(Socket, Result).
+
+
+gather(Res) ->
+    [Combine|_Rest] = Res,
+    [Key|_] = Combine,
+    AnotherKey = ets:next(person_info, Key),
+    if
+        AnotherKey == '$end_of_table' ->
+            Res;
+        true ->
+            [AnotherRes] = ets:lookup(person_info, AnotherKey),
+            gather([AnotherRes|Res])
+    end.
+%% 处理‘get’请求 (end)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %                                       init----->start
